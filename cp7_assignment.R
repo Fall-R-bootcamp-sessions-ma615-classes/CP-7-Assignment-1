6e4a1d723a98aaa1f19481b10e1d10b69431e275
aidanohara1 <aohara1@bu.edu>
2023-08-21 15:50:09 -0400

    ass temp and instruct

---
+++ cp7_assignment.R
@@ -0,0 +1,38 @@
+## YOUR NAME HERE ##
+
+library(vioplot)
+
+### Exercise 1
+
+# anotherMatrix <- 
+
+# use the apply function to calculate
+# anotherMean <- 
+# yetAnotherMean <- 
+# sortedRows <- 
+
+### Exercise 2
+
+#mtcars
+
+# make a list of the minimum values for each column of mtcars
+# l <- lapply()
+# s <- sapply()
+# m <- mapply()
+# listOfApplys <- list(l,s,m)
+# typesOfApplies <- sapply()
+
+### Exercise 3 # Titanic
+# Use the standard ‘Titanic’ dataset which is part of R Base
+
+# passengersBySex <- 
+# survivorsBySex <- 
+# passengersBySexByAge <- 
+
+### Exercise 4
+# Plotting with the ‘apply’ family. Use the dataset ‘iris’ from R Base.
+
+#box <- 
+# length(box) ==  4, just to double check your work
+
+#violin <- 
